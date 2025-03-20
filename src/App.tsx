17464d92a8f84dbba625f6a0d70d0001dc99a59b
Williams V <wibrvallejoma@gmail.com>
2025-03-19 20:40:32 -0600

    feat: implement todo management functions in App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,32 @@ import { AddTodo } from './components/addTodo';
 function App() {
   const [todos, setTodos] = useState<{ id: number; text: string; done: boolean}[]>([]);
 
+  const handleAdd = (text: string) => {
+    const newTodo = {
+      id: Date.now(),
+      text,
+      done: false,
+    };
+    setTodos([...todos, newTodo]);
+  }
+
+  const handleToggle = (id: number) => {
+    setTodos(todos.map(todo => {
+        if (todo.id === id) todo.done = !todo.done;
+        return todo;
+      })
+    )
+  }
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
 
   return (
     <>
       <Header title="To-do App" />
-      <AddTodo />
-      <TodoList todos={todos} />
+      <AddTodo onAdd={handleAdd} />
+      <TodoList todos={todos} onToggle={handleToggle} onDelete={handleDelete} />
     </>
   )
 }
