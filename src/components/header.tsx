1a7b3bfd71c498914adb4a4c83883705edf91edb
Williams V <wibrvallejoma@gmail.com>
2025-03-19 21:47:48 -0600

    add header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,8 @@
 export function Header({ title = 'Hello world' }) {	
   return (
     <div>
-      <h1>{ title }</h1>
-      <h2>Subtitle</h2>
+      <h1 className="text-3xl font-bold underline">{ title }</h1>
+      <h2 className="text-2xl">Subtitle</h2>
     </div>
   );
 }
